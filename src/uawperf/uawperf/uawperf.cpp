4c594ab687c54734b74f147cf52d1cfda50ff27c
tukotech <tuko.ustx@gmail.com>
2022-06-12 22:31:23 -0500

    Time var

--- src/uawperf/uawperf/uawperf.cpp
+++ src/uawperf/uawperf/uawperf.cpp
@@ -1,6 +1,50 @@
-#include <iostream>
+//#include <iostream>
 #include <signal.h>
 #include "open62541.h"
+
+static UA_StatusCode
+readCurrentTime(UA_Server* server,
+    const UA_NodeId* sessionId, void* sessionContext,
+    const UA_NodeId* nodeId, void* nodeContext,
+    UA_Boolean sourceTimeStamp, const UA_NumericRange* range,
+    UA_DataValue* dataValue) {
+    UA_DateTime now = UA_DateTime_now();
+    UA_Variant_setScalarCopy(&dataValue->value, &now,
+        &UA_TYPES[UA_TYPES_DATETIME]);
+    dataValue->hasValue = true;
+    return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode
+writeCurrentTime(UA_Server* server,
+    const UA_NodeId* sessionId, void* sessionContext,
+    const UA_NodeId* nodeId, void* nodeContext,
+    const UA_NumericRange* range, const UA_DataValue* data) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+        "Changing the system time is not implemented");
+    return UA_STATUSCODE_BADINTERNALERROR;
+}
+
+static void
+addCurrentTimeDataSourceVariable(UA_Server* server) {
+    UA_VariableAttributes attr = UA_VariableAttributes_default;
+    attr.displayName = UA_LOCALIZEDTEXT((char*)"en-US", (char*)"Current time - data source");
+    attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+
+    UA_NodeId currentNodeId = UA_NODEID_STRING(1, (char*)"current-time-datasource");
+    UA_QualifiedName currentName = UA_QUALIFIEDNAME(1, (char*)"current-time-datasource");
+    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
+    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
+    UA_NodeId variableTypeNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE);
+
+    UA_DataSource timeDataSource;
+    timeDataSource.read = readCurrentTime;
+    timeDataSource.write = writeCurrentTime;
+    UA_Server_addDataSourceVariableNode(server, currentNodeId, parentNodeId,
+        parentReferenceNodeId, currentName,
+        variableTypeNodeId, attr,
+        timeDataSource, NULL, NULL);
+}
 
 static volatile UA_Boolean running = true;
 static void stopHandler(int sig) {
@@ -33,6 +77,8 @@ int main() {
     UA_VariableAttributes_clear(&attr);
     UA_NodeId_clear(&myIntegerNodeId);
     UA_QualifiedName_clear(&myIntegerName);
+
+    addCurrentTimeDataSourceVariable(server);
 
     UA_StatusCode retval = UA_Server_run(server, &running);
 
